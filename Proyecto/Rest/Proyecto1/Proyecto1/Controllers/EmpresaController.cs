44ab4f1e26790468860d83390fd673f73e283809
ijtj03 <ijtj03@estudiantec.cr>
2017-10-06 10:59:06 -0600

    GET y POST de empresa

--- Proyecto/Rest/Proyecto1/Proyecto1/Controllers/EmpresaController.cs
+++ Proyecto/Rest/Proyecto1/Proyecto1/Controllers/EmpresaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Proyecto1.Classes;
+using Proyecto1.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +9,22 @@ using System.Web.Http;
 
 namespace Proyecto1.Controllers
 {
+    [RoutePrefix("api/Empresa")]
     public class EmpresaController : ApiController
     {
+        [HttpGet]
+        [Route("GetAllEmpresas")]
+        public IHttpActionResult GetAllSucursales()
+        {
+            EmpresaService con = new EmpresaService();
+            return Ok(con.GetAllEmpresas());
+        }
+        [HttpPost]
+        [Route("PostEmpresa")]
+        public void PostSucursal([FromBody] Empresa empresa)
+        {
+            EmpresaService con = new EmpresaService();
+            con.PostEmpresa(empresa);
+        }
     }
 }
---
+++ Proyecto/Rest/Proyecto1/Proyecto1/Services/EmpresaService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Proyecto1.Classes;
+using System.Web.Http;
+namespace Proyecto1.Services
+{
+    public class EmpresaService
+    {
+        public List<Empresa> GetAllEmpresas()
+        {
+            System.Data.SqlClient.SqlConnection conn;
+            SqlCommand command;
+            SqlDataReader read;
+
+            conn = new SqlConnection("Data Source=(local);Initial Catalog=Farmacia;Integrated Security=True");
+            conn.Open();
+            command = new SqlCommand("SELECT *  from Empresa", conn);
+            read = command.ExecuteReader();
+            List<Empresa> empresas = new List<Empresa>();
+            while (read.Read())
+            {
+                Empresa empresa = new Empresa();
+                empresa.IdEmpresa = Convert.ToInt32(read["IdEmpresa"]);
+                empresa.Nombre = read["Nombre"].ToString();
+                empresa.LogicDelete = Convert.ToBoolean(read["LogicDelete"]);
+                empresas.Add(empresa);
+
+            }
+            return empresas;
+        }
+        public void PostEmpresa([FromBody] Empresa empresa)
+        {
+            System.Data.SqlClient.SqlConnection conn;
+            SqlCommand command;
+
+            conn = new SqlConnection("Data Source=(local);Initial Catalog=Proyecto1;Integrated Security=True");
+            conn.Open();
+
+            command = new SqlCommand("insert  Empresa(Nombre) VALUES (@Nombre)", conn);
+
+            command.Parameters.AddWithValue("@Nombre", empresa.Nombre);
+            command.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+    }
+}
