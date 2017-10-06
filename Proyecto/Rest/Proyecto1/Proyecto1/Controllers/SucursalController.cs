34a882572e745837b2c7c5b08e5c57dfe1597e55
luisf1997cr <luismuelino@gmail.com>
2017-10-06 02:30:28 -0600

    Sucursak Service

--- Proyecto/Rest/Proyecto1/Proyecto1/Controllers/SucursalController.cs
+++ Proyecto/Rest/Proyecto1/Proyecto1/Controllers/SucursalController.cs
@@ -4,10 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Proyecto1.Services;
+using Proyecto1.Classes;
 
 namespace Proyecto1.Controllers
 {
+    [RoutePrefix("api/Sucursal")]
     public class SucursalController : ApiController
     {
+        [HttpGet]
+        [Route("GetAllSucursales")]
+        public IHttpActionResult GetAllSucursales()
+        {
+            SucursalService con = new SucursalService();
+            return Ok(con.GetAllSucursales());
+        }
+
+        [HttpPost]
+        [Route("PostSucursal")]
+        public void PostSucursal([FromBody] Sucursal sucursal)
+        {
+            SucursalService con = new SucursalService();
+            con.PostSucursal(sucursal);
+        }
     }
 }
---
+++ Proyecto/Rest/Proyecto1/Proyecto1/Services/SucursalService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Proyecto1.Classes;
+using System.Web.Http;
+using System.Data.SqlClient;
+
+namespace Proyecto1.Services
+{
+    public class SucursalService
+    {
+        public List<Sucursal> GetAllSucursales()
+        {
+            System.Data.SqlClient.SqlConnection conn;
+            SqlCommand command;
+            SqlDataReader read;
+
+            conn = new SqlConnection("Data Source=(local);Initial Catalog=Proyecto1;Integrated Security=True");
+            conn.Open();
+            command = new SqlCommand("SELECT *  from Sucursal", conn);
+            read = command.ExecuteReader();
+
+            List<Sucursal> ListSucursales = new List<Sucursal>();
+            while (read.Read())
+            {
+                Sucursal sucursal = new Sucursal();
+                sucursal.IdSucursal = Convert.ToInt32(read["IdSucursal"]);
+                sucursal.IdEmpresa = Convert.ToInt32(read["IdEmpresa"]);
+                sucursal.Administrador = Convert.ToInt32(read["Administrador"]);
+                sucursal.Nombre = read["Nombre"].ToString();
+                sucursal.Provincia = read["Provincia"].ToString();
+                sucursal.Canton = read["Canton"].ToString();
+                sucursal.Distrito = read["Distrito"].ToString();
+                sucursal.DescripcionDireccion = read["DescripcionDireccion"].ToString();
+                sucursal.LogicDelete = Convert.ToBoolean(read["LogicDelete"]);
+
+                ListSucursales.Add(sucursal);
+
+            }
+            read.Close();
+            conn.Close();
+            return ListSucursales;
+        }
+
+        public void PostSucursal([FromBody] Sucursal sucursal)
+        {
+            System.Data.SqlClient.SqlConnection conn;
+            SqlCommand command;
+
+            conn = new SqlConnection("Data Source=(local);Initial Catalog=Proyecto1;Integrated Security=True");
+            conn.Open();
+
+            SqlParameter IdSucursal = new SqlParameter("@IdSucursal", System.Data.SqlDbType.Int);
+            IdSucursal.Value = sucursal.IdSucursal;
+
+            SqlParameter IdEmpresa = new SqlParameter("@IdEmpresa", System.Data.SqlDbType.Int);
+            IdEmpresa.Value = sucursal.IdEmpresa;
+
+            SqlParameter Administrador = new SqlParameter("@Administrador", System.Data.SqlDbType.Int);
+            Administrador.Value = sucursal.Administrador;
+
+            SqlParameter Nombre = new SqlParameter("@Nombre", System.Data.SqlDbType.VarChar);
+            Nombre.Value = sucursal.Nombre;
+
+            SqlParameter Provincia = new SqlParameter("@Provincia", System.Data.SqlDbType.VarChar);
+            Provincia.Value = sucursal.Provincia;
+
+            SqlParameter Canton = new SqlParameter("@Canton", System.Data.SqlDbType.VarChar);
+            Canton.Value = sucursal.Canton;
+
+            SqlParameter Distrito = new SqlParameter("@Distrito", System.Data.SqlDbType.VarChar);
+            Distrito.Value = sucursal.Distrito;
+
+            SqlParameter DescripcionDireccion = new SqlParameter("@DescripcionDireccion", System.Data.SqlDbType.VarChar);
+            DescripcionDireccion.Value = sucursal.DescripcionDireccion;
+
+            SqlParameter LogicDelete = new SqlParameter("@LogicDelete", System.Data.SqlDbType.Bit);
+            LogicDelete.Value = sucursal.LogicDelete;
+
+            command = new SqlCommand("insert into Sucursal(IdSucursal,IdEmpresa,Administrador,Nombre,Provincia,Canton,Distrito,DescripcionDireccion,LogicDelete) VALUES (@IdSucursal,@IdEmpresa,@Administrador,@Nombre,@Provincia,@Canton,@Distrito,@DescripcionDireccion,@LogicDelete)", conn);
+            command.Parameters.Add(IdEmpresa);
+            command.Parameters.Add(IdSucursal);
+            command.Parameters.Add(Administrador);
+            command.Parameters.Add(Nombre);
+            command.Parameters.Add(Provincia);
+            command.Parameters.Add(Canton);
+            command.Parameters.Add(Distrito);
+            command.Parameters.Add(DescripcionDireccion);
+            command.Parameters.Add(LogicDelete);
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+    }
+}
