dc9b74ffe9c112c165721d5897f50184e1ac57bd
ijtj03 <ijtj03@estudiantec.cr>
2017-10-08 02:21:33 -0600

    Jalando sucursales para realizar un pedido

---
+++ Proyecto/Rest/Proyecto1/Proyecto1/Scripts - Views/RealizarPedidos.js
@@ -0,0 +1,9 @@
+﻿var rPedidos = angular.module('RPedidos', []);
+rPedidos.controller("rPedidosController", function ($scope, $http, $location) {
+    $http.get('http://localhost:64698/api/Sucursal/GetAllSucursales')
+        .then(function (response) {
+            console.log("Geting");
+            $scope.sucursales = response.data;
+            console.log("Geted");
+        });
+});
