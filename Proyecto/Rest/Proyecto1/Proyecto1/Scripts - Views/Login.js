c716c26d711a51ef16a20de5543c9f07b78f468e
Esteban Herrera V <nabetse.hv.09@gmail.com>
2017-10-07 20:32:16 -0600

    Para Isaac

--- Proyecto/Rest/Proyecto1/Proyecto1/Scripts - Views/Login.js
+++ Proyecto/Rest/Proyecto1/Proyecto1/Scripts - Views/Login.js
@@ -1,32 +1,32 @@
-﻿var app = angular.module('myApp', []);
-app.controller('myController', ['Route',function ($scope, $http) {
-    $scope.id = $scope.id;
-    $scope.password = $scope.password;
+﻿var app = angular.module('myApp', ['ngRoute'])
+   /* .config(function ($routeProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "main.html",
+                controller: "myController"
+            })
+            .when("/register", {
+                templateUrl: "register.html",
+                controller: "myController"
+            })
+    })*/
+app.controller('myController', function ($scope, $http) {
+        $scope.id = $scope.id;
+        $scope.password = $scope.password;
 
-    $scope.log = function (id, password) {
-        console.log("Ya entre");
-        console.log(id, password);
-        $http.get("http://localhost:64698/api/Persona/SignInVerification?id=" + id + "&" + "contraseña=" + password)
-            .then(function (response) {
-                $scope.res = response;
-                if (response.data == true) {
-                    console.log("Logged");
-                } else {
-                    console.log("The Id and password are not correct");
-                }
-            });
-
-
-    };
-}]);
-
-app.config("Route", function ($routeProvider) {
-    $routeProvider
-        .when("/", {
-            templateUrl: "main.html"
-        })
-        .when("/register", {
-            templateUrl: "register.html"
-        });
-
+        $scope.log = function (id, password) {
+            console.log("Ya entre");
+            console.log(id, password);
+            $http.get("http://localhost:64698/api/Persona/SignInVerification?id=" + id + "&" + "contraseña=" + password)
+                .then(function (response) {
+                    $scope.res = response;
+                    if (response.data == true) {
+                        console.log("Logged");
+                        
+                    } else {
+                        console.log("The Id and password are not correct");
+                    }
+                });
+        };
 });
+
