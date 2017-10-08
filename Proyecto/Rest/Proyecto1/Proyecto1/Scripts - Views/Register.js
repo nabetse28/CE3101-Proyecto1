e7b1cb0d77f998bcfa35571a18025451474ac293
Esteban Herrera V <nabetse.hv.09@gmail.com>
2017-10-08 00:46:13 -0600

    RegisterCliente

---
+++ Proyecto/Rest/Proyecto1/Proyecto1/Scripts - Views/Register.js
@@ -0,0 +1,41 @@
+﻿var register = angular.module('Register',[]);
+
+register.controller('registerController', function ($scope, $http) {
+    console.log("Register");
+    $scope.cedula = $scope.cedula;
+    $scope.nombre = $scope.nombre;
+    $scope.apellido1 = $scope.apellido1;
+    $scope.apellido2 = $scope.apellido2;
+    $scope.telefono = $scope.telefono;
+    $scope.provincia = $scope.provincia;
+    $scope.canton = $scope.canton;
+    $scope.distrito = $scope.distrito;
+    $scope.fecha = $scope.fecha;
+
+    
+
+    $scope.register = function () {
+        console.log("Hola");
+
+        var usuario = {
+            IdCedula: $scope.cedula,
+            Nombre: $scope.nombre,
+            Apellido1: $scope.apellido1,
+            Apellido2: $scope.apellido2,
+            Telefono: $scope.telefono,
+            Contraseña: $scope.password,
+            Provincia: $scope.provincia,
+            Canton: $scope.canton,
+            Distrito: $scope.distrito,
+            DescripcionDireccion: $scope.direccion,
+            FechaNacimiento: $scope.fecha,
+        }
+
+
+        $http.post("http://localhost:64698/api/Persona/PostPersona", usuario);
+
+        window.location = "http://localhost:64698/mywebsite/login.html";
+    
+    }
+
+});
